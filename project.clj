f7f08955d5fd61f91ad42faa863ebdb5917a0589
Madara Uchiha <me@madara.ninja>
2022-12-18 10:26:25 +0200

    lein

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject aoc-2022 "0.1.0-SNAPSHOT"
+  :description "Advent of Code 2022 - In Clojure"
+  :url "https://github.com/MadaraUchiha/aoc2022"
+  :license {:name "WTFPL"
+            :url "http://www.wtfpl.net/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :main ^:skip-aot aoc-2022.main
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
